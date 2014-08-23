3d3c44c4b1539e4af0dec15ec4596e36fcc2d7c4
rahul86s <rahul86s@gmail.com>
2014-08-23 14:47:14 +0530

    added job info

--- map-reduce/src/main/java/com/ts/mapreduce/index/IndexDriver.java
+++ map-reduce/src/main/java/com/ts/mapreduce/index/IndexDriver.java
@@ -2,21 +2,38 @@ package com.ts.mapreduce.index;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.record.Index;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class IndexDriver extends Configured implements Tool{
 
-	public int run(String[] arg0) throws Exception {
+	public int run(String[] args) throws Exception {
 		Configuration conf =getConf();
 		Job job = new Job(conf, "Indexing Job");
+		job.setMapOutputKeyClass(LongWritable.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setMapperClass(IndexMapper.class);
+		job.setReducerClass(IndexReucer.class);
+		job.setNumReduceTasks(1);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		job.setJarByClass(Index.class);
-		return 0;
+		return job.waitForCompletion(true)?0:1;
 	}
 	public static void main(String[] args) throws Exception {
 		Configuration config = new Configuration();
-		ToolRunner.run(config,new IndexDriver(), args);
+		System.exit(ToolRunner.run(config,new IndexDriver(), args));
 	}
 }
--- map-reduce/src/main/java/com/ts/mapreduce/index/IndexMapper.java
+++ map-reduce/src/main/java/com/ts/mapreduce/index/IndexMapper.java
@@ -10,7 +10,8 @@ public class IndexMapper extends Mapper<Object , Text, LongWritable, Text>{
 	@Override
 	protected void map(Object key, Text value, Mapper<Object, Text, LongWritable, Text>.Context context)
 			throws IOException, InterruptedException {
-		context.write(new LongWritable((Long) key), value);
+		System.out.println(key.toString());
+		context.write(new LongWritable(Long.parseLong(key.toString())), value);
 	}
 
 }
